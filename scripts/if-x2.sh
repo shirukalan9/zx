d538f83b640a313c71eded24e42b73595be91274
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-05-13 13:56:18 +0700

    bb

--- scripts/if-x2.sh
+++ scripts/if-x2.sh
@@ -2,12 +2,12 @@
 rm -rf device frameworks external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
 
 # ROM Init
-#repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault --depth=1 --git-lfs --no-repo-verify
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault --depth=1 --git-lfs --no-repo-verify
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
 # Device 
-git clone -b staging  https://github.com/testingprjct/8dua device/infinix/X6882
+git clone -b stable  https://github.com/testingprjct/8dua device/infinix/X6882
 
 # Build
 source build/envsetup.sh
