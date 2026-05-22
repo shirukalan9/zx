2317d7fe2129883b867b7f6eb2920c9d9f7e432b
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-05-22 10:47:17 +0700

    bs

--- scripts/lun-x2.sh
+++ scripts/lun-x2.sh
@@ -2,16 +2,16 @@
 rm -rf device frameworks external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
 
 # ROM Init
-#repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs --no-repo-verify
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs --no-repo-verify
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
 # Device 
-git clone -b axi  https://github.com/zaidannn7/device_infinix_X6882 device/infinix/X6882
+git clone -b luna https://github.com/zaidannn7/device_infinix_X6882 device/infinix/X6882
 
 # Build
 . build/envsetup.sh
 
 
-axion X6882 user vanilla
-ax -b
+lunch lineage_X6882-bp4a-user
+m bacon
