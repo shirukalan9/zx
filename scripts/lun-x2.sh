f29435df61b8207980ab31da387147e6d5a8cc53
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-05-23 11:13:26 +0700

    b

--- scripts/lun-x2.sh
+++ scripts/lun-x2.sh
@@ -1,5 +1,5 @@
 # Cleanup
-rm -rf device frameworks external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
+rm -rf device  hardware/mediatek device/mediatek  vendor/mediatek build/tools
 
 # ROM Init
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs --no-repo-verify
