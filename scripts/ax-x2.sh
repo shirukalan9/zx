ed8445fdb0733984d0b12880de65116bf8bfde64
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-06-03 11:42:59 +0700

    ua

--- scripts/ax-x2.sh
+++ scripts/ax-x2.sh
@@ -8,7 +8,7 @@ repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 /opt/crave/resync.sh
 
 # Device 
-git clone -b ax  https://github.com/testingprjct/8dua device/infinix/X6882
+git clone -b ax  https://github.com/zaidannn7/device_infinix_X6882_guts device/infinix/X6882
 
 # Build
 source build/envsetup.sh
