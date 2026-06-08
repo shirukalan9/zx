853121ec126b2d6c3e8e5257fc24b60c34ca3451
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-06-08 10:56:37 +0700

    j

--- scripts/lun-x2.sh
+++ scripts/lun-x2.sh
@@ -1,5 +1,6 @@
 # Cleanup
-rm -rf frameworks/native device/infinix
+rm -rf device/infinix
+#frameworks/native
 #ardware/mediatek device/mediatek  vendor/mediatek build/tools
 
 #error: Unable to fully sync the tree
@@ -12,7 +13,12 @@ rm -rf frameworks/native device/infinix
 # ROM Init
 repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --depth=1 --git-lfs --no-repo-verify
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+
 /opt/crave/resync.sh
+#from : https://github.com/sreepadmarat/buildscripts/blob/main/build.sh
+# user: https://foss.crave.io/app/#/build/info/278748?team=14
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
 
 # Device 
 git clone -b lunar https://github.com/zaidannn7/device_infinix_X6882_guts device/infinix/X6882
