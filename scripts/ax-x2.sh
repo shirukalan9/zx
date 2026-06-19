f084db5901f288c4a85ec293b94ea2c1a8e040cc
shirukalan9 <127198882+shirukalan9@users.noreply.github.com>
2026-06-20 04:20:31 +0700

    gq

--- scripts/ax-x2.sh
+++ scripts/ax-x2.sh
@@ -3,7 +3,6 @@
 #external tools hardware/ device/mediatek  vendor/mediatek hardware/mediatek android packages build prebuilts prebuilt
 #
 #
-rm device/xiaomi
 #
 #
 # ---------------------------------------------------------------------------
@@ -18,7 +17,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # Device 
 rm -rf device/infinix
-git clone -b ab  https://github.com/shirukalan9/zvt device/infinix/X6882
+git clone -b ax  https://github.com/zaidannn7/device_infinix_X6882-jva device/infinix/X6882
 
 # Build
 source build/envsetup.sh
